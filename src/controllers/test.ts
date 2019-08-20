7b8791928a6e7c66388dc745d9088dc55cef97f0
npnhannguyen <nhan.nguyen@netpower.no>
2019-08-20 17:35:34 +0700

    Reset

--- src/controllers/test.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-
-export let index = (req: Request, res: Response) => {
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ a: 1 }));
-  };
