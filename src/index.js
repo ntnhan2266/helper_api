d8bbf5da899a0858bf107f9c68f2c96cafd68c60
npnhannguyen <nhan.nguyen@netpower.no>
2019-09-06 17:33:37 +0700

    Change port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const userRouter = require('./routers/user')
 const authRouter = require('./routers/auth')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.use(express.json())
 app.use(userRouter)
