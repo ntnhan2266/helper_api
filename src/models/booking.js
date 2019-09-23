5eac4cbb8fd1af7218cff5d4996135561ac7bd38
nhannguyen <macbook@MacBooks-MBP.net.fpt>
2019-09-24 00:03:48 +0700

    Booking model

---
+++ src/models/booking.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const bookingSchema = new mongoose.Schema(
+  {
+    type: {
+      type: Number,
+      required: true
+    },
+    address: {
+      type: String,
+      required: true
+    },
+    houseNumber: {
+      type: String,
+      required: true
+    },
+    startDate: {
+      type: Date,
+      required: true
+    },
+    endDate: {
+      type: Date,
+      required: false
+    },
+    startTime: {
+      type: Date,
+      required: true
+    },
+    endTime: {
+      type: Date,
+      required: true
+    },
+    note: {
+      type: String,
+      required: false
+    },
+    lat: {
+      type: Number,
+      required: true
+    },
+    long: {
+      type: Number,
+      required: true
+    },
+    maid: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Maid"
+    },
+    interval: {
+      type: Map,
+      of: Boolean,
+      required: false
+    }
+  },
+  { timestamps: true }
+);
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+module.exports = Booking;
