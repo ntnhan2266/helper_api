a47ca7f9e98bd77380c7e91c0db23bfd9dcd3627
npnhannguyen <nhan.nguyen@netpower.no>
2019-10-16 17:29:40 +0700

    Get reviews of  maid

--- src/routers/review.js
+++ src/routers/review.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const Review = require("../models/review");
+const Maid = require('../models/maid')
 const Booking = require("../models/booking");
 const authMiddleware = require("../middleware/auth");
 const router = new express.Router();
-var mongoose = require('mongoose');
-var _ = require('lodash');
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 router.post("/review", authMiddleware, async (req, res) => {
   try {
@@ -30,8 +31,34 @@ router.post("/review", authMiddleware, async (req, res) => {
   }
 });
 
+router.get('/reviews', async (req, res) => {
+  const pageSize = req.query.pageSize || 10;
+  const pageIndex = req.query.pageIndex || 0;
+  const maidId = req.query.maidId;
+  const reviews = await Review.find({ maid: maidId })
+    .skip(pageIndex * pageSize)
+    .limit(pageSize)
+    .sort([['createdAt', -1]]).exec();
+  const maidIds = reviews.map(review => {
+    console.log(review);
+    return mongoose.Types.ObjectId(review.maid);
+  });
+  let maids = await Maid.find({ _id: { $in: maidIds } }).populate({
+    path: "user",
+    select: "name avatar birthday gender phoneNumber address"
+  });
+  maids = _.keyBy(maids, "_id");
+  for (let i = 0; i < reviews.length; i++) {
+    if (maids[reviews[i].maid]) {
+      reviews[i].maid = maids[reviews[i].maid];
+    }
+  }
+  const total = await Review.countDocuments({maid: maidId});
+  res.send({ reviews, total});
+});
+
 router.get('/reviews', authMiddleware, async (req, res) => {
-  
+
 });
 
 module.exports = router;
