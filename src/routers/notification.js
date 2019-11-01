2e41ac09b3084b2f2f430ba5c0fd2a13c64b1a5a
Hien Le Duy <hienld.citynow@gmail.com>
2019-11-02 01:14:24 +0700

    add  notification

--- src/routers/notification.js
+++ src/routers/notification.js
@@ -9,35 +9,16 @@ router.get("/notification", authMiddleware, async (req, res) => {
   try {
     const pageSize = req.query.pageSize ? req.query.pageSize * 1 : 12;
     const pageIndex = req.query.pageIndex ? req.query.pageIndex * 1 : 0;
-    const requestUser = req.user;
-    const notifications = await Notification.find()
-      .populate({
-        path: "booking",
-        match: {
-          $or: [
-            { createdBy: requestUser._id },
-            { maid: requestUser._id },
-          ]
-        },
-        populate: [
-          {
-            path: "maid",
-            populate: {
-              path: "user",
-              select: "name"
-            }
-          }, {
-            path: "createdBy",
-            select: "name"
-          }
-        ]
-      })
-      .sort({updatedAt: -1})
+    const notifications = await Notification.find({
+      toUser: req.user._id
+    })
+      .populate("booking", '-createdBy -maid')
+      .populate("fromUser")
+      .sort({ updatedAt: -1 })
       .skip(pageIndex * pageSize)
       .limit(pageSize);
     res.send({ notifications });
   } catch (e) {
-    console.log(e);
     res.send({
       errorCode: 1,
       errorMessage: "Can not get notification"
