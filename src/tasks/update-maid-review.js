e20af033d431c54fe2bc03ec82f1229ee3af9f03
npnhannguyen <nhan.nguyen@netpower.no>
2019-12-05 10:58:12 +0700

    Cron update maid ratting

--- src/tasks/update-maid-review.js
+++ src/tasks/update-maid-review.js
@@ -1,9 +1,26 @@
-const cron = require('node-cron');
+const cron = require("node-cron");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const Maid = require('../models/maid');
-const Review = require('../models/review');
+const Maid = require("../models/maid");
+const Review = require("../models/review");
 
-cron.schedule('0 0 * * *', () => {
-    
+cron.schedule("* * * * *", async () => {
+  try {
+    // Get all maid
+    const maids = await Maid.find();
+    for (let maid of maids) {
+      // Get all reviews of maid
+      const numberOfRatting = await Review.countDocuments({ maid });
+      const avgRatting = await Review.aggregate([
+        { $match: { maid: maid._id } },
+        { $unwind: "$rating" },
+        { $group: { _id: maid._id, average: { $avg: "$rating" } } }
+      ]);
+      maid.numberOfRatting = numberOfRatting;
+      maid.ratting = avgRatting[0].average;
+      maid.save();
+    }
+  } catch (e) {
+    console.log(e);
+  }
 });
